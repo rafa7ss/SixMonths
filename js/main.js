1b997b4011cb72c9891a2d048bb4434bc4f42c0f
Rafael Martins <rafaelmartinsdandrade@gmail.com>
2021-10-22 23:19:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function startTimer(duration, display) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         if (--timer > 0) {
-            display.innerHTML = '<button>Tô pronta!</button>';
+            display.innerHTML = "<input type='button'>Tô pronta!</button>";
             // display.textContent = dias +" dias, "+horas + ":" + minutes + ":" + seconds;
         }else{
             display.textContent = '<button>Tô pronta!</button>';
