1aeaf854ed390ce6c7166e415b6e06486879c853
Rafael Martins <rafaelmartinsdandrade@gmail.com>
2021-10-23 00:52:53 -0300

    Loader e Timer- CSS + JS 100%
    Implementação de Loader, Timer e suas funções.
    
    Obs: Falta adicionar logo centralizada no Loader.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    setInterval(function () {
+    var timerInterval = setInterval(function () {
         dias = parseInt(timer / 60 / 60 / 24, 10);
         horas = parseInt(timer / 60 / 60 % 24, 10);
         minutes = parseInt(timer / 60 % 60, 10);
@@ -8,17 +8,20 @@ function startTimer(duration, display) {
         horas = horas == 0 ? "00" : horas;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-        if (--timer > 0) {
-            document.getElementById('box-timer').innerHTML = "<button type='button' class='btn btn-primary'>Tô pronta!</button>";
-            // display.textContent = dias +" dias, "+horas + ":" + minutes + ":" + seconds;
+        if (timer != 0) {
+            display.textContent = dias +" dias, "+horas + ":" + minutes + ":" + seconds;
         }else{
-            display.textContent = '<button>Tô pronta!</button>';
+            display.textContent = dias +" dias, "+horas + ":" + minutes + ":" + seconds;
+            document.getElementById('btnTrans').classList.remove('disabled');
+            clearInterval(timerInterval);
         }
+        timer--;
     }, 1000);
 }
 
 function configTimer() {
 
+    var display = document.querySelector('#timer'); // selecionando o timer
     var data = new Date(), mesN, anoN, diferenca;
 
     var dia = data.getDate();
@@ -29,10 +32,14 @@ function configTimer() {
     var strDia = String(data.getDate()).padStart(2, '0');
     var strMes = String(data.getMonth() + 1).padStart(2, '0');
     var strHoje = strDia + '/' + strMes + '/' + ano;
+
     if(dia < 5){
         mesN = mes;
     }else{
         mesN = mes + 1;
+        display.textContent = "0 dias, 00:00:00";
+        setTimeout(() => {document.getElementById('btnTrans').classList.remove('disabled');}, 2000);
+        return;
     }
     if(mes != 12){
         anoN = ano;
@@ -41,14 +48,21 @@ function configTimer() {
     }
 
     var aniversario = new Date(String(mesN).padStart(2, '0')+"/05/"+String(anoN).padStart(2, '0')+" 00:00:00");
+    var aniversario = new Date(String(10).padStart(2, '0')+"/23/"+String(anoN).padStart(2, '0')+" 00:22:00");
     var timeDiff = Math.abs(aniversario.getTime() - hoje.getTime());
     var duration = Math.ceil(timeDiff / (1000));
-    display = document.querySelector('#timer'); // selecionando o timer
     startTimer(duration, display); // iniciando o timer
 }
 
 function loader(){
     loader = document.querySelector('#loader');
-    setTimeout(() => {loader.classList.add("trans");}, 500);
+    cover = document.querySelector('#cover');
+    setTimeout(() => {cover.classList.remove("trans");}, 800);
+    setTimeout(() => {loader.classList.add("trans");}, 1000);
 }
 
+function transicao(){
+    cover = document.querySelector('#cover');
+    setTimeout(() => {cover.classList.add("trans");}, 500);
+    setTimeout(() => {content.classList.add("visible");}, 500);
+}
